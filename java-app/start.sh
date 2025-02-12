92988703b5887bbb349b68ca4619d9ba5cbe6d29
shivam086r <106476217+shivam086r@users.noreply.github.com>
2025-02-12 20:25:50 +0530

    Create start.sh

---
+++ java-app/start.sh
@@ -0,0 +1,8 @@
+mvn clean package
+cd ./target
+
+# To run the app with no instrumentation:
+# java -jar app-1.0-SNAPSHOT.jar
+
+# To run the app with instrumentation:
+ java -javaagent:./newrelic/newrelic.jar -jar app-1.0-SNAPSHOT.jar
